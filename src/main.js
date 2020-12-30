6d7abe60313ab539e237f8c148888099d67ed6f1
huyue <huyue@aixuexi.com>
2020-12-30 14:25:02 +0800

    feat:init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import vconsole from "vconsole";
+import 'lib-flexible/flexible.js'
+ 
+new vconsole();
 
-createApp(App).mount('#app')
+const app = createApp(App)
+ 
+app.use(router)
+ 
+app.mount('#app')
